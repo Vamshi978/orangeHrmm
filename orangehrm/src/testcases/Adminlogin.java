1705c16464a6a022cece26776cd1c3f5fb7abd47
ramesh <ramesh@DESKTOP-1NURSPO>
2026-05-25 23:39:53 +0530

    adminelogin updated

--- orangehrm/src/testcases/Adminlogin.java
+++ orangehrm/src/testcases/Adminlogin.java
@@ -3,5 +3,6 @@ package testcases;
 public class Adminlogin {
 public static void main(String[] args) {
 	System.out.println("test");
+	System.out.println("test1");
 }
 }
---
+++ orangehrm/src/testcases/admine2.java
@@ -0,0 +1,7 @@
+package testcases;
+
+public class admine2 {
+public static void main(String[] args) {
+	System.out.println("syso");
+}
+}
